07a495a30cd539d632d646036749526e400263db
atsuyaide <kappa_deatsu@yahoo.co.jp>
2023-10-02 23:01:04 +0900

    バリデーションとロード画面の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Backdrop, CircularProgress } from "@mui/material";
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/ Form";
 import Icons from "./components/Icons";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
   return (
     <>
       <Icons />
-      <Form />
+      <Form setLoading={setLoading} />
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </>
   );
 }
--- src/components/ Form.tsx
+++ src/components/ Form.tsx
@@ -1,5 +1,7 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, MenuItem, Stack, TextField } from "@mui/material";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
 
 interface TzOption {
   value: string;
@@ -13,38 +15,79 @@ interface FormInput {
   fmt: string;
 }
 
-const Form = () => {
-  const { register, handleSubmit } = useForm<FormInput>(); // 使用したいメソッド等
+const TzOptions: TzOption[] = [
+  { value: "JST", label: "JST" },
+  { value: "UTC", label: "UTC" },
+];
+const FormatOptions: FormatOption[] = [
+  { 書式: "yyyy-MM-dd" },
+  { 書式: "yyyy-MM-dd HH:mm:SS" },
+];
+const schema = yup.object({
+  tz: yup.string().required(), // 何も指定しなければデフォルトのエラー文が表示される.
+  fmt: yup.string().required("必須項目です."), // エラー文を指定できる.
+});
+
+interface FormProps {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Form = (props: FormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>({
+    resolver: yupResolver(schema),
+  });
   const onSubmit: SubmitHandler<FormInput> = (data) => {
+    props.setLoading(true);
     console.log(data);
-    // google.script.run
-    //   .withSuccessHandler((result: string) => {
-    //     alert(`現在は${result}です.`);
-    //   })
-    //   .withFailureHandler((e: any) => {
-    //     if (e instanceof Error) {
-    //       alert("実行に失敗しました.");
-    //     }
-    //   })
-    //   .getDateTimeNow("JST", "yyyy-MM-dd HH:mm:ss");
+    try {
+      google.script.run
+        .withSuccessHandler((result: string) => {
+          alert(`現在は${result}です.`);
+          props.setLoading(false);
+        })
+        .withFailureHandler((e: any) => {
+          if (e instanceof Error) {
+            alert("実行に失敗しました.");
+          }
+          props.setLoading(false);
+        })
+        .getDateTimeNow(data.tz, data.fmt);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(`失敗しました: ${error.message}`);
+      }
+      setTimeout(() => {
+        props.setLoading(false);
+      }, 2000);
+    }
   };
-  const TzOptions: TzOption[] = [
-    { value: "JST", label: "JST" },
-    { value: "UTC", label: "UTC" },
-  ];
-  const FormatOptions: FormatOption[] = [
-    { 書式: "yyyy-mm-dd" },
-    { 書式: "yyyy-mm-dd HH:MM:SS" },
-  ];
   return (
     <>
       <Stack spacing={2}>
-        <TextField select label="タイムゾーン" {...register("tz")}>
+        <TextField
+          select
+          required
+          error={"tz" in errors}
+          helperText={errors.tz?.message}
+          label="タイムゾーン"
+          {...register("tz")}
+        >
           {TzOptions.map((opt) => (
             <MenuItem value={opt.value}>{opt.label}</MenuItem>
           ))}
         </TextField>
-        <TextField select required label="書式" {...register("fmt")}>
+        <TextField
+          select
+          required
+          error={"fmt" in errors}
+          helperText={errors.fmt?.message}
+          label="書式"
+          {...register("fmt")}
+        >
           {FormatOptions.map((opt) => (
             <MenuItem value={opt.書式}>{opt.書式}</MenuItem>
           ))}
